67d670f0fa3d0d255f14ba9bd3af770fd655f32b
JoneHyttinen <jone.hyttinen@myy.haaga-helia.fi>
2026-02-01 22:29:40 +0200

    Implemented Book model class, BookController which handles get request to path /index, and added Thymeleaf template index.html

--- src/main/java/hh/bookstore/sof3/domain/Book.java
+++ src/main/java/hh/bookstore/sof3/domain/Book.java
@@ -1,5 +1,65 @@
 package hh.bookstore.sof3.domain;
 
 public class Book {
+    String title;
+    String author;
+    int publicationYear;
+    String isbn;
+    float price;
 
+    public Book() {
+        title = null;
+        author = null;
+        publicationYear = 0;
+        isbn = null;
+        price = 0.0F;
+    }
+
+    public Book(String title, String author, int publicationYear, String isbn, float price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
--- src/main/java/hh/bookstore/sof3/web/BookController.java
+++ src/main/java/hh/bookstore/sof3/web/BookController.java
@@ -1,5 +1,26 @@
 package hh.bookstore.sof3.web;
 
-public class BookController {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.bookstore.sof3.domain.Book;
+
+@Controller
+public class BookController {
+    private List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        books.add(new Book("The Myth of Sisyphus", "Albert Camus", 1942, "978-0-525-56445-4", 15.30F));
+        books.add(new Book("To Your Eternity 1", "Yoshitoki Oima", 2017, "978-1-63236-571-2", 13.40F));
+    }
+
+    @GetMapping("/index")
+    public String getBooks(Model model) {
+        model.addAttribute("books", books);
+        return "index"; // index.html
+    }
 }
